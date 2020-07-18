4ccc6dec6b62a8c222e20ec26e1d32c86d22b243
Yancey Leo <yanceyofficial@gmail.com>
2020-07-19 01:59:49 +0800

    feat: learn dns

--- src/dns/index.ts
+++ src/dns/index.ts
@@ -1,21 +1,58 @@
-import dns from 'dns'
-import { YANCEY_DOMIN, BAIDU_DOMAIN } from '../shared/constants'
+import { promises as dnsPromises } from 'dns'
+import { BAIDU_DOMAIN, GOOGLE_DOMAIN, YANCEY_DOMIN } from '../shared/constants'
 
-// dns.lookup(BAIDU_DOMAIN, (err, address, family) => {
-//   console.log('地址: %j 地址族: IPv%s', address, family)
-// })
-
-dns.resolve4('baidu.com', (err, addresses) => {
-  if (err) throw err
-
-  console.log(`地址: ${JSON.stringify(addresses)}`)
-
-  addresses.forEach((a) => {
-    dns.reverse(a, (err, hostnames) => {
-      if (err) {
-        throw err
-      }
-      console.log(`地址 ${a} 逆向到: ${JSON.stringify(hostnames)}`)
+//
+const getLookUp = async (url: string) => {
+  try {
+    const { address, family } = await dnsPromises.lookup(url, {
+      family: 0, // 记录的地址族: 0 | 4 | 6
+      hints: 0,
+      all: false,
+      verbatim: true,
     })
-  })
-})
+    console.log(`地址: ${address} 地址族: IPv${family}s`)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+// 地址: 104.31.78.7 地址族: IPv4s
+getLookUp(BAIDU_DOMAIN)
+
+// 使用 DNS 协议解析 IPv4 地址
+const getResolve4 = async (url: string) => {
+  try {
+    const addresses = await dnsPromises.resolve4(url)
+    console.log(addresses)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+// [ '216.58.200.238' ]
+getResolve4(GOOGLE_DOMAIN)
+
+// 使用 DNS 协议解析 IPv6 地址
+const getResolve6 = async (url: string) => {
+  try {
+    const addresses = await dnsPromises.resolve6(url)
+    console.log(addresses)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+// [
+//   '2606:4700:3035::681f:4e07',
+//   '2606:4700:3034::681f:4f07',
+//   '2606:4700:3032::ac43:df01'
+// ]
+getResolve6(YANCEY_DOMIN)
+
+const getDomainFromIP = async (ip: string) => {
+  try {
+    const hostnames = await dnsPromises.reverse(ip)
+    console.log(hostnames)
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+// [ 'yanceyleo.com' ]
+getDomainFromIP('39.156.69.79')
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,5 @@
-export const YANCEY_DOMIN = 'https://yanceyleo.com'
-export const GOOGLE_DOMAIN = 'https://google.com'
-export const BAIDU_DOMAIN = 'https://baidu.com'
+export const PROTOCOL = 'https://'
+
+export const YANCEY_DOMIN = 'yanceyleo.com'
+export const GOOGLE_DOMAIN = 'google.com'
+export const BAIDU_DOMAIN = 'baidu.com'
